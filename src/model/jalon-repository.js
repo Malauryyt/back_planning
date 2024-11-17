e6e9a86d80284790267c3cea48ad7651651a1d09
Malauryyt <110236103+Malauryyt@users.noreply.github.com>
2024-11-17 22:58:35 +0100

    début modification

--- src/model/jalon-repository.js
+++ src/model/jalon-repository.js
@@ -155,3 +155,114 @@ exports.modifDateCom = async (id_jalon, date_com, charge) =>{
         return 0;
     }
 }
+
+exports.modifJalon = async (id_jalon, libelle, date_liv_prev, date_commencement = "", id_user, id_projet) => {
+
+    const tabJalon = await this.getJalonByProjet(id_projet);
+    var prece = 0;
+    var dateCommencement = "";
+    var dateLivPrece = "";
+    var idJalonAModif = 0;
+    var finLivJalonAModif = "";
+    var infoJalons = "";
+
+    // on récupère les anciennes valeurs
+    infoJalons = tableau.find(item => item.id_jalon === 10);
+
+    // Suppression de mon jalon pour faciliter la modification
+    console.log(tabJalon);
+    const filteredTableau = tabJalon.filter(item => item.id_jalon !== id_jalon);
+    console.log("tab jalon sans celui en modif", tabJalon);
+
+    // si il y a plusieur jalon on regarde ou se place notre jalon
+    if(tabJalon != 0 && tabJalon.length > 0){
+
+        for (const jalon of tabJalon) {
+
+            if(jalon.date_liv_theorique == date_liv_prev ){
+                return 0;
+            }
+            if( date_liv_prev < jalon.date_liv_theorique && prece == 0){
+
+                idJalonAModif = jalon.id_jalon;
+                finLivJalonAModif = jalon.date_liv_theorique
+                dateCommencement = dateLivPrece;
+                prece += 1;
+            }
+            dateLivPrece = jalon.date_liv_theorique;
+        }
+        //si c'est le dernier jalon
+        if(prece == 0 ){
+            dateCommencement = dateLivPrece;
+        }
+
+    }
+
+    // modification de mon jalon
+    async function modifJalon(id_jalon, libelle, date_liv_prev, date_commencement, id_user, etat) {
+        try{
+            const jalonModif = await sequelize.query(`UPDATE "jalon" 
+                                            SET libelle = :libelle,
+                                                date_liv_theorique= :date_liv_prev,
+                                                date_com_theorique= : date_commencement
+                                                id_user = :id_user,
+                                                etat = :etat
+                                            WHERE id_jalon = :id_jalon ;`, { replacements: {  libelle, date_liv_prev,
+                                                                                                    date_commencement,  id_user, etat, id_jalon}})
+                .then(([results, metadata]) => {
+                    console.log("Modification jalon effectuée.", results);
+                });
+
+            return 1;
+        } catch (error) {
+            console.error('Erreur lors de la modification du jalon:', error);
+            return 0;
+        }
+    }
+
+    // prise en compte de la date commmencement que si notre jalon devient le premier de la liste
+    if( date_commencement != ""){
+        dateCommencement = date_commencement
+    }
+    else{
+        //Calcule de la date de commencement ( on y ajoute un jour)
+        const date = new Date(dateCommencement);
+        date.setDate(date.getDate() + 1);
+
+        // Retourner la nouvelle date au format 'YYYY-MM-DD'
+        dateCommencement =  date.toISOString().split('T')[0];
+    }
+
+    // création de mon nouveau jalon
+    const nouvJalon = createJalon(libelle, date_liv_prev, dateCommencement, id_user, id_projet);
+    if(nouvJalon == 0 ){
+        return 0;
+    }
+
+    // modification de la date de commencement d'un jalon
+    if( idJalonAModif != 0){
+        //ReCalcule de la date de commencement ( on y ajoute un jour)
+        const date = new Date(date_liv_prev);
+        date.setDate(date.getDate() + 1);
+
+        // Retourner la nouvelle date au format 'YYYY-MM-DD'
+        date_liv_prev =  date.toISOString().split('T')[0];
+
+        // Recalcule de la charge en jour
+        const start = new Date(date_liv_prev);
+        const end = new Date(finLivJalonAModif);
+        const diffTime = end - start;
+
+        // Convertir la différence en jours
+        const diffDays = diffTime / (1000 * 60 * 60 * 24);
+        const charge =  Math.round(diffDays);
+
+        const modif =  await this.modifDateCom(idJalonAModif, date_liv_prev, charge )
+        if(modif != 1){
+            return 0;
+        }
+    }
+
+    return 1;
+
+};
